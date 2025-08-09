5e982cf46e37e51b57bba44f5f236b505450b400
poojarivinod <vinod07081@gmail.com>
2025-08-09 15:37:23 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,19 @@
+variable "instances" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+  default = "Z0802349255PD9KZJ7SQF"
+}
+
+variable "domain_name" {
+  default = "poojari.store"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    project      = "expense"
+    environment = "dev"
+  }
+}
