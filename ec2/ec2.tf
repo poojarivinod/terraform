1a15b83d0d7e2c297b91ae2f3a3de9a92dc5324c
poojarivinod <vinod07081@gmail.com>
2026-01-20 13:01:37 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "web" {                    #terraform aws ec2
-  ami                    = "ami-09c813fb71547fc4f" # this is our devops-practice AMI id
+  # count = 2
+  ami                    = "ami-0220d79f3f480ecf5" # this is our devops-practice AMI id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
@@ -10,7 +11,7 @@ resource "aws_instance" "web" {                    #terraform aws ec2
 
 resource "aws_security_group" "allow_tls" { #terraform aws security group
   name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow SSH inbound traffic and all outbound traffic"
   ingress {
     from_port   = 22
     to_port     = 22
